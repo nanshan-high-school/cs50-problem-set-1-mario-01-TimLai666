36671492af031436b00a28f7c7a61402c720aa89
TimLai666 <43640816+TimLai666@users.noreply.github.com>
2018-10-19 22:44:01 +0800

    增加功能
    1.現在會分別顯示各種幣值的硬幣數量
    2.可以重複輸入

--- money.cpp
+++ money.cpp
@@ -1,33 +1,40 @@
 #include <iostream>
 using namespace std;
 int main() {
-  int fifty = 50;
-  int ten = 10;
-  int five = 5;
-  int one = 1;
-  int num = 0;
-  int l=0;
-  int total;
-  cout << "輸入總金額\n";
-  cin >> total;
-  if ( total >= fifty ) {
-    num = num + total / fifty;
-    l = total % fifty;
-  }else {
-    l = total;
+  while ( true ) {
+    int fifty = 50;
+    int ten = 10;
+    int five = 5;
+    int one = 1;
+    int numfifty = 0;
+    int numten = 0;
+    int numfive = 0;
+    int numone = 0;
+    int totalnum = 0;
+    int l=0;
+    int total;
+    cout << "請輸入總金額\n";
+    cin >> total;
+    if ( total >= fifty ) {
+      numfifty = total / fifty;
+      l = total % fifty;
+    }else {
+      l = total;
+    }
+    if ( l >= ten ) {
+      numten = l / ten;
+      l = l % ten;
+    }
+    if ( l >= five ) {
+      numfive = l / five;
+      l = l % five;
+    }
+    if ( l >= one ) {
+      numone = l / one;
+      l = l % one;
+    }
+    totalnum = numfifty + numten + numfive + numone;
+    cout << "最少硬幣數:" << totalnum << "\n(" << fifty << "元" << numfifty << "枚、" << ten << "元" << numten << "枚、" << five << "元" << numfive << "枚、" << one << "元" << numone << "枚)\n\n";
   }
-  if ( l >= ten ) {
-    num = num + l / ten;
-    l = l % ten;
-  }
-  if ( l >= five ) {
-    num = num + l / five;
-    l = l % five;
-  }
-  if ( l >= one ) {
-    num = num + l / one;
-    l = l % one;
-  }
-  cout << "最少硬幣數:" << num;
-  }
+}
   
